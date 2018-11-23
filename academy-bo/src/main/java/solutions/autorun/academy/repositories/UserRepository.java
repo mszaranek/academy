df86d911b2bfe05b7c2975dd0345f05d71505138
PiotrSzczepan <35820191+PiotrSzczepan@users.noreply.github.com>
2018-11-23 09:21:12 +0100

    conflict resolve

--- academy-bo/src/main/java/solutions/autorun/academy/repositories/UserRepository.java
+++ academy-bo/src/main/java/solutions/autorun/academy/repositories/UserRepository.java
@@ -9,34 +9,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
  
     @EntityGraph(value="userEntityGraph")
     List<User> findAll();
 
    @EntityGraph(value="userEntityGraph")
     Optional<User> findById(Long aLong);
-
-    //Optional<User> findOneByActivationKey(String activationKey);
-
-    //List<User> findAllByActivatedIsFalseAndCreatedDateBefore(Instant dateTime);
-
-    //Optional<User> findOneByResetKey(String resetKey);
-
-    Optional<User> findOneByEmailIgnoreCase(String email);
-
-    Optional<User> findOneByUsername(String username);
-
-    @EntityGraph(attributePaths = "appRoles")
-    Optional<User> findOneWithAppRolesById(Long id);
-
-    @EntityGraph(attributePaths = "appRoles")
-    Optional<User> findOneWithAppRolesByUsername(String username);
-
-    @EntityGraph(attributePaths = "appRoles")
-    Optional<User> findOneWithAppRolesByEmail(String email);
-
-    Page<User> findAllByUsernameNot(Pageable pageable, String username);
 
     @EntityGraph(value="userEntityGraph")
     Optional<User> findByEmail(String email);
